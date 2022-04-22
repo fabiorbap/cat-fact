06e61d0c2f5d639affbb6908b67619984b855fb9
Fabio Rachid <fabiorbap@gmail.com>
2022-04-21 23:06:01 -0300

    Merge conflicts

--- app/src/main/java/br/fabiorachid/catfact/model/datasources/FactsRemoteDataSource.kt
+++ app/src/main/java/br/fabiorachid/catfact/model/datasources/FactsRemoteDataSource.kt
@@ -1,4 +1,10 @@
 package br.fabiorachid.catfact.model.datasources
 
+import br.fabiorachid.catfact.model.data.FactApiResponse
+import io.reactivex.Single
+
 interface FactsRemoteDataSource {
+
+    fun getFact(): Single<FactApiResponse>
+
 }
--- app/src/main/java/br/fabiorachid/catfact/model/datasources/FactsRemoteDataSourceImpl.kt
+++ app/src/main/java/br/fabiorachid/catfact/model/datasources/FactsRemoteDataSourceImpl.kt
@@ -1,6 +1,12 @@
 package br.fabiorachid.catfact.model.datasources
 
 import br.fabiorachid.catfact.model.ApiInterface
+import br.fabiorachid.catfact.model.data.FactApiResponse
+import io.reactivex.Single
 
 class FactsRemoteDataSourceImpl (val api: ApiInterface) : FactsRemoteDataSource {
+
+    override fun getFact(): Single<FactApiResponse> {
+        TODO("Not yet implemented")
+    }
 }
